1118b7933aadcfaba1b02e0eb93d2f449fcfe6d1
Kurt Petrola <kurtguenpetrola2.0@gmail.com>
2026-03-24 00:04:34 +0800

    refactor: extract PomodoroSettings into settings/domain

--- lib/features/settings/application/settings_controller.dart
+++ lib/features/settings/application/settings_controller.dart
@@ -1,45 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/services/storage_service.dart';
+import '../domain/pomodoro_settings.dart';
 
-class PomodoroSettings {
-  final int workDurationMinutes;
-  final int shortBreakDurationMinutes;
-  final int longBreakDurationMinutes;
-  final String themeColor;
-  final bool enableNotifications;
-  final bool confirmBeforeNextTimer;
-  final bool playSoundWhenCompleted;
-
-  const PomodoroSettings({
-    required this.workDurationMinutes,
-    required this.shortBreakDurationMinutes,
-    required this.longBreakDurationMinutes,
-    this.themeColor = 'Green',
-    this.enableNotifications = true,
-    this.confirmBeforeNextTimer = true,
-    this.playSoundWhenCompleted = true,
-  });
-
-  PomodoroSettings copyWith({
-    int? workDurationMinutes,
-    int? shortBreakDurationMinutes,
-    int? longBreakDurationMinutes,
-    String? themeColor,
-    bool? enableNotifications,
-    bool? confirmBeforeNextTimer,
-    bool? playSoundWhenCompleted,
-  }) {
-    return PomodoroSettings(
-      workDurationMinutes: workDurationMinutes ?? this.workDurationMinutes,
-      shortBreakDurationMinutes: shortBreakDurationMinutes ?? this.shortBreakDurationMinutes,
-      longBreakDurationMinutes: longBreakDurationMinutes ?? this.longBreakDurationMinutes,
-      themeColor: themeColor ?? this.themeColor,
-      enableNotifications: enableNotifications ?? this.enableNotifications,
-      confirmBeforeNextTimer: confirmBeforeNextTimer ?? this.confirmBeforeNextTimer,
-      playSoundWhenCompleted: playSoundWhenCompleted ?? this.playSoundWhenCompleted,
-    );
-  }
-}
+export '../domain/pomodoro_settings.dart';
 
 final settingsControllerProvider = NotifierProvider<SettingsController, PomodoroSettings>(SettingsController.new);
 final settingsProvider = settingsControllerProvider;
---
+++ lib/features/settings/domain/pomodoro_settings.dart
@@ -0,0 +1,43 @@
+class PomodoroSettings {
+  final int workDurationMinutes;
+  final int shortBreakDurationMinutes;
+  final int longBreakDurationMinutes;
+  final String themeColor;
+  final bool enableNotifications;
+  final bool confirmBeforeNextTimer;
+  final bool playSoundWhenCompleted;
+
+  const PomodoroSettings({
+    required this.workDurationMinutes,
+    required this.shortBreakDurationMinutes,
+    required this.longBreakDurationMinutes,
+    this.themeColor = 'Green',
+    this.enableNotifications = true,
+    this.confirmBeforeNextTimer = true,
+    this.playSoundWhenCompleted = true,
+  });
+
+  PomodoroSettings copyWith({
+    int? workDurationMinutes,
+    int? shortBreakDurationMinutes,
+    int? longBreakDurationMinutes,
+    String? themeColor,
+    bool? enableNotifications,
+    bool? confirmBeforeNextTimer,
+    bool? playSoundWhenCompleted,
+  }) {
+    return PomodoroSettings(
+      workDurationMinutes: workDurationMinutes ?? this.workDurationMinutes,
+      shortBreakDurationMinutes:
+          shortBreakDurationMinutes ?? this.shortBreakDurationMinutes,
+      longBreakDurationMinutes:
+          longBreakDurationMinutes ?? this.longBreakDurationMinutes,
+      themeColor: themeColor ?? this.themeColor,
+      enableNotifications: enableNotifications ?? this.enableNotifications,
+      confirmBeforeNextTimer:
+          confirmBeforeNextTimer ?? this.confirmBeforeNextTimer,
+      playSoundWhenCompleted:
+          playSoundWhenCompleted ?? this.playSoundWhenCompleted,
+    );
+  }
+}
